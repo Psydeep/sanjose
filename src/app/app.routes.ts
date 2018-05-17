7ec38cd91ef20070685ef2000821a29db5e47213
Psydeep <junior-rk5@hotmail.com>
2018-05-17 03:26:27 -0500

    update 1.04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,9 @@ import { PagenofoundComponent } from './shared/pagenofound/pagenofound.component
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: '**', component: PagenofoundComponent }
+    //{ path: '**', component: PagenofoundComponent },
+    {path: '404', component: PagenofoundComponent},
+    {path: '**', redirectTo: '404'}
 ];
 
 export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
